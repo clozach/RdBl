6fd906263d06e7f229801e9fa8801e3895f96baf
Chris Lozac'h <clozach@gmail.com>
2019-11-01 14:41:07 -0700

    Ensure Svelte app floats above all other content
    I suspect this is also a good design decision for other, not-yet-apparent reasons. If nothing else, it's nice to be able to pick it out more easily in the DOM, thanks to the `rdbl-svelte-app` id.

--- rdbl-svelte/src/main.js
+++ rdbl-svelte/src/main.js
@@ -1,7 +1,15 @@
 import App from './App.svelte';
 
+const target = document.createElement("div")
+target.id = "rdbl-svelte-app"
+target.style.position = "absolute"
+// Most of the time we're invisible, but when we _aren't_
+// we'd better damn well 👇 be on top, or what's the point?!
+target.style.zIndex = "10000"
+document.body.prepend(target)
+
 const app = new App({
-	target: document.body,
+	target: target,
 	props: {
 		name: 'world'
 	}
